e45213640d204183c9f270e33a27fffd494d9d2d
Mike Aizatsky <mike.aizatsky@gmail.com>
2013-10-31 21:20:43 -0700

    Processor class for defining observers as operations on observables.

---
+++ src/main/scala/com/mikea/gae/rx/base/Processor.scala
@@ -0,0 +1,23 @@
+package com.mikea.gae.rx.base
+
+
+/**
+ * @author mike.aizatsky@gmail.com
+ */
+abstract class Processor[T] extends Observer[T] {
+  def process(observable: Observable[T]): Unit
+
+  def instantiate[C](aClass: Class[C]): C
+
+  private val observable: PushObservable[T] = new PushObservable[T] {
+    def instantiate[C](aClass: Class[C]) = Processor.this.instantiate(aClass)
+  }
+
+  process(observable)
+
+  def onCompleted() = observable.onCompleted()
+
+  def onError(e: Exception) = observable.onError(e)
+
+  def onNext(value: T) = observable.onNext(value)
+}
