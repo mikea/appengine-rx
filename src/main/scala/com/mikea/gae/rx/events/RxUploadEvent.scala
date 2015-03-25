651dd0c0c5a5ec0eae7c72abed54ed441e5742b7
Mike Aizatsky <mike.aizatsky@gmail.com>
2015-03-25 12:12:46 -0700

    Build fix

--- src/main/scala/com/mikea/gae/rx/events/RxUploadEvent.scala
+++ src/main/scala/com/mikea/gae/rx/events/RxUploadEvent.scala
@@ -1,7 +1,7 @@
 package com.mikea.gae.rx.events
 
 import com.google.appengine.api.blobstore.BlobInfo
-import com.google.common.base.Objects
+import com.google.common.base.{MoreObjects, Objects}
 import com.mikea.gae.rx.Rx
 
 /**
@@ -9,7 +9,7 @@ import com.mikea.gae.rx.Rx
  */
 class RxUploadEvent(event: RxHttpRequest, _blobInfos: Map[String, Set[BlobInfo]]) extends RxHttpRequest(event) {
   override def toString: String = {
-    Objects.toStringHelper(this).add("blobInfos", _blobInfos).toString
+    MoreObjects.toStringHelper(this).add("blobInfos", _blobInfos).toString
   }
 
   def blobInfos() : Map[String, Set[BlobInfo]] = _blobInfos
--- src/main/scala/com/mikea/gae/rx/events/RxVersionUpdateEvent.scala
+++ src/main/scala/com/mikea/gae/rx/events/RxVersionUpdateEvent.scala
@@ -1,10 +1,10 @@
 package com.mikea.gae.rx.events
 
-import com.google.common.base.Objects
+import com.google.common.base.{MoreObjects, Objects}
 
 /**
  * @author mike.aizatsky@gmail.com
  */
 class RxVersionUpdateEvent(version: String) {
-  override def toString = Objects.toStringHelper(this).add("version", version).toString
+  override def toString = MoreObjects.toStringHelper(this).add("version", version).toString
 }
